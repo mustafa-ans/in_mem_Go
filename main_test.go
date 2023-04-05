3e5aaf2ac2eb7be606a84c18647149129616e6f5
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2023-04-05 22:14:50 +0530

    refactor

--- main_test.go
+++ main_test.go
@@ -6,38 +6,6 @@ import (
     "time"
 )
 
-func benchmarkQPush(b *testing.B, numValues int) {
-    data := &datastore{data: make(map[string]*dataValue)}
-    key := "testkey"
-
-    // Generate values
-    values := make([]string, numValues)
-    for i := 0; i < numValues; i++ {
-        values[i] = strconv.Itoa(i)
-    }
-
-    // Run benchmark
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        err := data.qPush(key, values...)
-        if err != nil {
-            b.Fatal(err)
-        }
-    }
-}
-
-func BenchmarkQPush10(b *testing.B) {
-    benchmarkQPush(b, 10)
-}
-
-func BenchmarkQPush100(b *testing.B) {
-    benchmarkQPush(b, 100)
-}
-
-func BenchmarkQPush1000(b *testing.B) {
-    benchmarkQPush(b, 1000)
-}
-
 func TestGetValue(t *testing.T) {
     data := &datastore{data: make(map[string]*dataValue)}
 
@@ -73,3 +41,34 @@ func TestGetValue(t *testing.T) {
 
 }
 
+func benchmarkQPush(b *testing.B, numValues int) {
+    data := &datastore{data: make(map[string]*dataValue)}
+    key := "testkey"
+
+    // Generate values
+    values := make([]string, numValues)
+    for i := 0; i < numValues; i++ {
+        values[i] = strconv.Itoa(i)
+    }
+
+    // Run benchmark
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        err := data.qPush(key, values...)
+        if err != nil {
+            b.Fatal(err)
+        }
+    }
+}
+
+func BenchmarkQPush10(b *testing.B) {
+    benchmarkQPush(b, 10)
+}
+
+func BenchmarkQPush100(b *testing.B) {
+    benchmarkQPush(b, 100)
+}
+
+func BenchmarkQPush1000(b *testing.B) {
+    benchmarkQPush(b, 1000)
+}

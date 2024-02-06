e206031dc2fd7464852ceece4c8e1cfb5d40adde
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2024-02-06 17:31:36 +0530

    removed unused code

--- datastore.go
+++ datastore.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-    "sync"
-    "github.com/sirupsen/logrus")
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
 
 type datastore struct {
-    data map[string]*dataValue
-    mu   sync.RWMutex
-    maxSizeBytes int // Maximum size
-    sizeBytes    int // Current size
-    logger *logrus.Logger
-    lruCacheHead *LRUCacheNode
-    lruCacheTail *LRUCacheNode
-    lruCacheMap  map[string]*LRUCacheNode
+	data         map[string]*dataValue
+	mu           sync.RWMutex
+	maxSizeBytes int // Maximum size
+	logger       *logrus.Logger
+	// sizeBytes    int // Current size
+	// lruCacheHead *LRUCacheNode
+	// lruCacheTail *LRUCacheNode
+	// lruCacheMap  map[string]*LRUCacheNode
 }
 
 type dataValue struct {
-    value    string
-    expTime  int64 // Expiry time in Unix timestamp format
-    isExists bool  // Used to check existence of key for conditional set operation
-    queueMu sync.Mutex // Mutex for the queue field
-    queue   []string
+	value    string
+	expTime  int64      // Expiry time in Unix timestamp format
+	isExists bool       // Used to check existence of key for conditional set operation
+	queueMu  sync.Mutex // Mutex for the queue field
+	queue    []string
 }
 
 type LRUCacheNode struct {
-    key   string
-    value string
-    prev  *LRUCacheNode
-    next  *LRUCacheNode
+	key   string
+	value string
+	prev  *LRUCacheNode
+	next  *LRUCacheNode
 }
